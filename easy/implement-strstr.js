e860c0e0c34a88c6b948c79fa3175fb92c050aee
tongxin2013 <609891732@qq.com>
2020-11-24 15:16:30 +0800

    feat：append 28. Implement strStr()

---
+++ easy/implement-strstr.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  for (var i = 0; i <= haystack.length - needle.length; i++) {
+    if (haystack.substr(i, needle.length) === needle) return i
+  }
+  return -1
+};
